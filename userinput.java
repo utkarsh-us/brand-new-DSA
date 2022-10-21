ae55154d033b65f044597218d5509550c5c2a4d2
sumit kumar <85924848+akasumitlamba@users.noreply.github.com>
2022-10-21 21:27:29 +0530

    jvac code

---
+++ userinput.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+class userinput {
+    public static void main(String[ ] args) {
+        Scanner myVar = new Scanner(System.in);
+        System.out.println(myVar.nextLine());        
+    }
+}
