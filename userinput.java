534d55f7c0f6ca00652ec9a857186cf4d47b3750
secretagentj5 <116306488+secretagentj5@users.noreply.github.com>
2023-10-10 02:09:13 +0530

    Delete userinput.java

--- userinput.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;
-class userinput {
-    public static void main(String[ ] args) {
-        Scanner myVar = new Scanner(System.in);
-        System.out.println(myVar.nextLine());        
-    }
-}
